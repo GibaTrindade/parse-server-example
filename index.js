34e27c732f84d51fa16cf7b49f120ea17bf0f73a
Gilberto Trindade <g.trindade@gmail.com>
2016-02-20 03:37:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a web site.');
+  res.status(200).send('<h2>Parse Server No Heroku.</h2>');
 });
 
 var port = process.env.PORT || 1337;
